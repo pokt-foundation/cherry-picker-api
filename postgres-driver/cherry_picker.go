f52966d6414c419af70c08959dd382156029d302
Roniel Valdez <roniel_valdez@outlook.com>
2022-05-31 15:42:52 -0400

    feat: add base driver and a query

---
+++ postgres-driver/cherry_picker.go
@@ -0,0 +1,34 @@
+package postgresdriver
+
+// Session model of the aggregated data of cherry picker of all regions
+type Session struct {
+	PublicKey          string  `db:"public_key"`
+	Chain              string  `db:"chain"`
+	SessionKey         string  `db:"session_key"`
+	Address            string  `db:"address"`
+	SessionHeight      int     `db:"session_height"`
+	TotalSuccess       int     `db:"total_success"`
+	TotalFailure       int     `db:"total_failure"`
+	AverageSuccessTime float64 `db:"avg_success_time"`
+	Failure            bool    `db:"failure"`
+}
+
+// Region model of data of cherry picker for a single region
+type Region struct {
+	PublicKey                 string    `db:"public_key"`
+	Chain                     string    `db:"chain"`
+	SessionKey                string    `db:"session_key"`
+	Region                    string    `db:"region"`
+	Address                   string    `db:"address"`
+	SessionHeight             int       `db:"session_height"`
+	TotalSuccess              int       `db:"total_success"`
+	TotalFailure              int       `db:"total_failure"`
+	MedianSuccessLatency      []float32 `db:"median_success_latency"`
+	WeightedSuccessLatency    []float32 `db:"weighted_success_latency"`
+	P90Latency                []float32 `db:"p_90_latency"`
+	SuccessRate               []float32 `db:"success_rate"`
+	Attempts                  []int     `db:"attempts"`
+	AvgSuccessLatency         float32   `db:"avg_success_latency"`
+	AvgWeightedSuccessLatency float32   `db:"avg_weighted_success_latency"`
+	Failure                   bool      `db:"failure"`
+}
---
+++ postgres-driver/postgres_driver.go
@@ -0,0 +1,56 @@
+package postgresdriver
+
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	getNodeSessionFromKey = `SELECT * FROM cherry_picker_session WHERE public_key = $1 
+		AND session_key = $2`
+	getNodeSessionFromHeight = `SELECT * FROM cherry_picker_session WHERE public_key = $1 
+		AND session_height = $2`
+)
+
+var (
+	// ErrMissingPublicKey when public key is empty
+	ErrMissingPublicKey = errors.New("missing publicKey field")
+	// ErrMissingSessionKeyAndHeight when session key and height fields are empty
+	ErrMissingSessionKeyAndHeight = errors.New("missing sessionKeight and sessionheight fields")
+)
+
+type PostgresDriver struct {
+	*sqlx.DB
+}
+
+// NewPostgresDriverFromConnectionString returns PostgresDriver instance from connection string
+func NewPostgresDriverFromConnectionString(connectionString string) (*PostgresDriver, error) {
+	db, err := sqlx.Open("postgres", connectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgresDriver{
+		DB: db,
+	}, nil
+}
+
+// NewPostgresDriverFromSQLDBInstance returns PostgresDriver instance from sdl.DB instance
+// mostly used for mocking tests
+func NewPostgresDriverFromSQLDBInstance(db *sql.DB) *PostgresDriver {
+	return &PostgresDriver{
+		DB: sqlx.NewDb(db, "postgres"),
+	}
+}
+
+func (pg *PostgresDriver) GetNodeSessionFromHeight(publicKey string, sessionHeight int) (*Session, error) {
+	var session Session
+	return &session, pg.Get(&session, getNodeSessionFromHeight, publicKey, sessionHeight)
+}
+
+func (pg *PostgresDriver) GetNodeSessionFromKey(publicKey, sessionKey string) (*Session, error) {
+	var session Session
+	return &session, pg.Get(&session, getNodeSessionFromHeight, publicKey, sessionKey)
+}
