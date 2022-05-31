0cf7e399733947edaca7975acc1e01660f9b6cce
Roniel Valdez <roniel_valdez@outlook.com>
2022-05-31 16:15:37 -0400

    feat: add base server config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pokt-foundation/cherry-picker-api/shared/environment"
+)
+
+var (
+	port = ":" + environment.GetString("port", "4200")
+)
 
 func main() {
-	fmt.Println("la mama de la mama de la mama de la mama de la mama de la mama de la mama de la mama de la mama de la mama")
+
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "si",
+		})
+	})
+	err := router.Run(port)
+	if err != nil {
+		log.Fatalf("error running server: %s", err.Error())
+	}
 }
