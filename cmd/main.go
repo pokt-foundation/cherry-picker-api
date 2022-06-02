1882ae60846c7a8da0ff9af65bcb3f7360e3886b
Roniel Valdez <roniel_valdez@outlook.com>
2022-06-02 11:48:48 -0400

    feat: db connection setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pokt-foundation/cherry-picker-api/database"
 	"github.com/pokt-foundation/cherry-picker-api/shared/environment"
 )
 
 var (
-	port = ":" + environment.GetString("port", "4200")
+	port               = ":" + environment.GetString("PORT", "4200")
+	dbConnectionString = environment.GetString("DB_CONNECTION_STRING", "")
 )
 
 func main() {
+	_, err := database.NewPostgresDriverFromConnectionString(dbConnectionString)
+	if err != nil {
+		log.Fatalf("cannot connect to database: %s", err.Error())
+	}
 
 	router := gin.Default()
 	router.GET("/", func(ctx *gin.Context) {
@@ -20,7 +27,9 @@ func main() {
 			"message": "si",
 		})
 	})
-	err := router.Run(port)
+
+	fmt.Printf("Server live and running on port %s\n", port)
+	err = router.Run(port)
 	if err != nil {
 		log.Fatalf("error running server: %s", err.Error())
 	}
